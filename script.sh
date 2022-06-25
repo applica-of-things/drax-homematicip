b6b0a399ef6a0af8d827dec1025d725a31b46725
Giuseppe Disimino <giuseppe.disimino@applica.guru>
2022-06-25 10:28:19 +0200

    added init script

---
+++ script.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+rootFolder="/root/bin"
+hmFolder=${rootFolder}/drax-hmIp
+
+if [ ! -d $rootFolder ]
+then
+        echo "Creating folder..."
+        mkdir -p $rootFolder
+fi
+
+cd $rootFolder
+
+if [ -d $hmFolder ]
+then
+        echo "removing existing folder..."
+        rm -rf $hmFolder
+fi
+
+wget https://github.com/applica-of-things/drax-homematicip/archive/refs/heads/main.zip
+
+unzip main.zip
+
+rm main.zip
+mv drax-homematicip-main drax-hmIp
+rm -rf drax-homematicip-main main.zip
+cd drax-hmIp
+npm install
+
+while true
+do
+        node main.js "https://draxcloud.com/trv/"
+        /bin/sleep 5
+done
