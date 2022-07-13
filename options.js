144f50540f860ceae8abdf47ce2d5074d02e76ee
Giuseppe Disimino <giuseppe.disimino@applica.guru>
2022-07-13 15:44:24 +0200

    options modified

--- options.js
+++ options.js
@@ -1,6 +1,6 @@
-const configPath = "/home/giuseppe/git/drax-homematicip/root/config.json"
-const keysPath = "/home/giuseppe/git/drax-homematicip/root/keys.json"
-const relaysPath = "/home/giuseppe/git/drax-homematicip/root/relays.json"
+const configPath = "/root/config.json"
+const keysPath = "/root/keys.json"
+const relaysPath = "/root/relays.json"
 module.exports = {
     configPath,
     keysPath,
