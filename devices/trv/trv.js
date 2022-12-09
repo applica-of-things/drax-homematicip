d16cfd70dc01de2fd2d5eec3fbedcdd1809c241b
Giuseppe Disimino <giuseppe.disimino@applica.guru>
2022-12-09 09:50:52 +0100

    ver 1.11

--- devices/trv/trv.js
+++ devices/trv/trv.js
@@ -150,7 +150,7 @@ class Trv extends GenericDevice {
         }
         if (data.SET_POINT_TEMPERATURE != null && data.SET_POINT_TEMPERATURE != undefined){
             let _state = new Config().instance().checkState("trv", this.address, data.SET_POINT_TEMPERATURE);
-            if (_state !== null && _state !== VALID_STATE){
+            if (_state !== null && _state !== VALID_STATE && data.WINDOW_STATE != 1){
                 this.setTargetTemperature(_state)
                 state.targetTemperature = _state
                 state.level = 0
--- devices/wth/wth.js
+++ devices/wth/wth.js
@@ -233,7 +233,7 @@ class Wth extends GenericDevice {
 
                 if (data.SET_POINT_TEMPERATURE != null && data.SET_POINT_TEMPERATURE != undefined){
                     let _state = new Config().instance().checkState("wth", this.address, data.SET_POINT_TEMPERATURE);
-                    if (_state !== null && _state !== VALID_STATE){
+                    if (_state !== null && _state !== VALID_STATE && data.WINDOW_STATE != 1){
                         this.setTargetTemperature(_state)
                     }
                 }
